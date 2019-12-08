0088c19e174847f3dd709c5b470e20eca1e15378
Hailey Gaylor <haileygaylor@gmail.com>
2019-12-08 15:02:16 -0800

    set up app with state. need to incorporate redux"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,2 +1,37 @@
 import React from 'react';
-import Header from 'Header';
+import Menu from './Menu';
+import Home from './Home';
+import {Switch, Route} from 'react-router-dom';
+import Error404 from './Error404';
+import RiverSearch from './RiverSearch';
+
+
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            masterRiverSearch: []
+        };
+        this.handleNewSearch = this.handleNewSearch.bind(this);
+    }
+        handleNewSearch(newSearch) {
+        var newMasterRiverSearch = this.state.masterRiverSearch.slice();
+        newMasterRiverSearch.push(newSearch);
+        this.setState({ masterRiverSearch: newMasterRiverSearch });
+    }
+    render(){
+        
+        return (
+            <div>
+            <Menu/>
+            <Switch/>
+            <Route exact path ='/' component={Home} />
+            <Route path ='/RiverSearch' render={() =><RiverSearch riverSearch={this.state.masterRiverSearch} currentRouterPath= {props.currentLocation.pathname}/>} /> 
+            <Route component={Error404} />
+            </div>
+        );
+    };
+}
+export default App;
